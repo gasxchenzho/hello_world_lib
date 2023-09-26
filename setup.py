f42942c61bb50848765fea44bed945559df49f38
gasxchenzho <77746930+gasxchenzho@users.noreply.github.com>
2023-09-25 22:38:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hello_world_lib",
+    name="helloworldlib",
     version="0.1",
     packages=find_packages(),
     author="Your Name",
